3182425bb7714719aa8c0738437a48137ac9ac29
franklin.r.castillo.3@gmail.com <franklin.r.castillo.3@gmail.com>
2024-10-19 09:24:59 -0400

    removed preferences files

--- content/preferences.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function () {
-  const prefs = Zotero.Prefs;
-  const optionOneCheckbox = document.getElementById('placeholder');
-
-  optionOneCheckbox.checked = prefs.get('extensions.zotero-books-to-sections.placeholder', true);
-  optionOneCheckbox.addEventListener('change', function (e) {
-    prefs.set('extensions.zotero-books-to-sections.placeholder', e.target.checked);
-  });
-});
