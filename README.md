bffac97c3ee752c9ed227ffff394b2e5f0ec04f1
franklin.r.castillo.3@gmail.com <franklin.r.castillo.3@gmail.com>
2024-10-19 23:18:13 -0400

    added outline text file feature

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ This is a work in progress.
 The primary motivation for developing this plugin was to easily split up edited books with chapters written by multiple authors, making it easier to reference these later on.
 
 ## To do:
-1. As a user, in the case that a PDF doesn't have an outline or if the outline isn't accurate, I want to write in a text box the titles and pages to create the book sections.
+1. Think of items for to-do list.
--- zotero-books-to-sections.js
+++ zotero-books-to-sections.js
@@ -1,20 +1,173 @@
-function _create(doc, name) {
+function _create(doc, name)
+{
   const elem = Zotero.platformMajorVersion >= 102
              ? doc.createXULElement(name)
              : doc.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", name);
   return elem;
 }
 
-function log(msg, err) {
+function log(msg, err)
+{
   Zotero.debug("Books To Sections: " + msg);
 
-  if (err) {
+  if (err)
+  {
     log(`Message: ${err.message}`);
     
-    err.stack.split('\n').forEach((line, idx) => {
+    err.stack.trim().split('\n').forEach((line, idx) => 
+    {
       log(`Stack: ${idx + 1}: ${line}`);
     });
   }
+}
+
+function FilePathToURL(path) {
+  return Zotero.isWin ? 'file:///' + path.replace(/\\/g, '/').replace(/ /g, '%20')
+                      : 'file://' + path.replace(/ /g, '%20');
+}
+
+async function writeFile(path, content)
+{
+  log(`Writing to file ${path}`);
+}
+
+async function parseOutlineFile(path, bookmarks)
+{
+  let text = await Zotero.File.getContentsAsync(path);
+  let rows = text.split('\n');
+
+  for (let row of rows)
+  {
+    if (!row.trim()) continue;
+
+    let rowMatch = row.match(/^"?(.*?)"?\s*,\s*(\d+)$/);
+
+    if (!rowMatch) continue;
+
+    let title = rowMatch[1];
+    let page  = parseInt(rowMatch[2]);
+
+    if (title[0] == '#') continue;
+
+    let elem = { title, page }
+
+    bookmarks.push(elem);
+  }
+}
+
+async function getBookmarksFromOutline(pdfDocument, outlineItems, store = [], maxDepth = 1)
+{
+	for (let item of outlineItems)
+  {
+		if (!item.items || item.items.length === 0 || maxDepth == 1)
+	  {
+			if (typeof item.dest === 'object' && item.dest[0]?.num !== undefined)
+		  {
+				let page = await pdfDocument.getPageIndex(item.dest[0]) + 1;
+				
+				store.push({title: item.title, page});
+				continue;
+			}
+			
+			if (typeof item.dest === 'string')
+		  {
+				let [ref, fitType, ...args] = await pdfDocument.getDestination(item.dest); 
+				let page = await pdfDocument.getPageIndex(ref) + 1;
+				let elem = {title: item.title, page};
+
+				store.push(elem);
+				continue;
+			}
+		}
+		else
+	  {
+			await getBookmarksFromOutline(pdfDocument, item.items, store, maxDepth - 1);
+		}
+	}
+}
+
+async function createBookSection(title, parentItem, collectionId)
+{
+  let bookSection = new Zotero.Item('bookSection');
+
+  bookSection.setField('title'    , title);
+  bookSection.setField('bookTitle', parentItem.getField('title'));
+
+  let fieldIdx = Zotero.ItemFields.getItemTypeFields(bookSection.itemTypeID);
+  let fields   = fieldIdx.map(idx => Zotero.ItemFields.getName(idx));
+
+  for (let field of fields) {
+    if (!['title', 'bookTitle'].includes(field)) {
+      let value = parentItem.getField(field);
+
+      bookSection.setField(field, value);
+    }
+  }
+  
+  await bookSection.saveTx();
+
+  bookSection.addToCollection(collectionId);
+
+  await bookSection.saveTx();
+
+  return bookSection;
+}
+
+async function addAttachment(parentItemID, title, path)
+{
+  log(`Attaching file ${path}`);
+
+  let attachment; 
+  
+  if (/^(https?|ftp|zotero):\/\//i.test(path))
+  {
+    attachment = new Zotero.Item('attachment');
+    attachment.setField('title', title);
+    attachment.setField('url', path);
+    attachment.parentID = parentItemID;
+    attachment.attachmentLinkMode = Zotero.Attachments.LINK_MODE_LINKED_URL;
+    await attachment.saveTx();
+    return;
+  }
+
+  let file = Zotero.File.pathToFile(path);
+
+  if (!file || !file.exists())
+  {
+    log(`File ${path} does not exist. Quitting.`);
+    return;
+  }
+
+  log(`File exists: ${path}`);
+
+  let attachArgs = { file, parentItemID, title };
+
+  attachment = await Zotero.Attachments.linkFromFile(attachArgs);
+
+  if (!attachment)
+  {
+    log(`Failed to create file ${path}`);
+    return;
+  }
+  
+  await attachment.saveTx();
+}
+
+async function getAttachment(item, title)
+{
+  let attachments = item.getAttachments();
+
+  for (let attachmentId of attachments)
+  {
+    let attachment = await Zotero.Items.getAsync(attachmentId);
+
+    if (attachment.getField('title') === title)
+    {
+      return attachment;
+    }
+  }
+
+  return null;
 }
 
 BooksToSections = {
@@ -25,7 +178,8 @@ BooksToSections = {
   initialized     : false,
   addedElementIDs : [],
 
-  init({ id, version, rootURI, pdfjsLib }) {
+  init({ id, version, rootURI, pdfjsLib })
+  {
     if (this.initialized) return;
     this.id          = id;
     this.version     = version;
@@ -34,221 +188,216 @@ BooksToSections = {
     this.initialized = true;
   },
 
-  async createBookSection(title, parentItem, collectionId) {
-    let bookSection = new Zotero.Item('bookSection');
-
-    bookSection.setField('title'    , title);
-    bookSection.setField('bookTitle', parentItem.getField('title'));
-
-    let fieldIdx = Zotero.ItemFields.getItemTypeFields(bookSection.itemTypeID);
-    let fields   = fieldIdx.map(idx => Zotero.ItemFields.getName(idx));
-
-    for (let field of fields) {
-      if (!['title', 'bookTitle'].includes(field)) {
-        let value = parentItem.getField(field);
-
-        bookSection.setField(field, value);
-      }
-    }
-    
-    await bookSection.saveTx();
-
-    bookSection.addToCollection(collectionId);
-
-    await bookSection.saveTx();
-
-    return bookSection;
-  },
-
-  async addAttachment(itemId, url) {
-    let attachment = new Zotero.Item('attachment');
-    
-    attachment.setField('title'   , url);
-    attachment.setField('url'     , url);
-    attachment.parentID = itemId;
-    attachment.attachmentLinkMode = Zotero.Attachments.LINK_MODE_LINKED_URL;
-
-    await attachment.saveTx();
-  },
-
-  async getBookmarks(pdfDoc, outlineItems, store = [], maxdepth = 1) {
-	  for (let item of outlineItems) {
-		  if (!item.items || item.items.length === 0 || maxdepth == 1) {
-			  if (typeof item.dest === 'object' && item.dest[0]?.num !== undefined) {
-				  let page = await pdfDoc.getPageIndex(item.dest[0]) + 1;
-				  
-				  store.push({title: item.title, page});
-				  continue;
-			  }
-			  
-			  if (typeof item.dest === 'string') {
-				  let [ref, fitType, ...args] = await pdfDoc.getDestination(item.dest); 
-				  let page = await pdfDoc.getPageIndex(ref) + 1;
-				  
-				  store.push({title: item.title, page});
-				  continue;
-			  }
-		  } else {
-			  await BooksToSections.getBookmarks(pdfDoc, item.items, store, maxdepth - 1);
-		  }
-	  }
-  },
-
-  addToWindow(window) {
+  addToWindow(window)
+  {
     let doc = window.document;
 
-    // Check if menu already exists
-    if (doc.getElementById("separate-chapters-menuitem")) {
-      return;
-    }    
+    // check if menu already exists
+    if (doc.getElementById("separate-chapters-menuitem")) return;    
 
-    // Get Zotero's item context menu
+    // get zotero's item context menu
     let menu = doc.getElementById('zotero-itemmenu');
 
-    // Create new menu item
-    let menuitem = _create(doc, "menuitem");
+    // create new menu item
+    let menuItem = _create(doc, "menuitem");
 
-    // Set menu item attributes
-    menuitem.setAttribute('id'   , 'separate-chapters-menuitem');
-    menuitem.setAttribute('label', 'Book to Book Sections');
+    // set menu item attributes
+    menuItem.setAttribute('id'   , 'separate-chapters-menuitem');
+    menuItem.setAttribute('label', 'Books to Book Sections');
 
-    // Run when clicked
-    menuitem.addEventListener('command', async function() {
-      let zpane    = Zotero.getActiveZoteroPane();
-      let items    = zpane.getSelectedItems();
-      let selCol   = zpane.getSelectedCollection();
-      let progress = new Zotero.ProgressWindow({ closeOnClick: true, modal: true });;
+    // run when clicked
+    menuItem.addEventListener('command', async function()
+    {
+      // show progress bar
+      let progress = new Zotero.ProgressWindow({ closeOnClick: false, modal: true });
 
       progress.changeHeadline('Books to Book Sections');
-      progress.show();
-      
+
       let progressItem = new progress.ItemProgress();
+      
       progressItem.setProgress(0);
       progressItem.setText(`Processing item...`);
 
-      try {
-        for (let item of items) {
+      progress.show();
+
+      let zpane = Zotero.getActiveZoteroPane();
+      let selectedItems      = zpane.getSelectedItems();
+      let selectedCollection = zpane.getSelectedCollection();
+      let outlineFileName    = 'outline-b2bs.txt'
+
+      try
+      {
+        for (let item of selectedItems)
+        {
           let bookItemId  = item.id;
+          let itemTitle   = item.getField('title');
           let attachments = item.getAttachments();
 
-          for (attachItemId of attachments) {
-            log(`Starting Item: ${attachItemId}`);
+          for (attachItemId of attachments)
+          {
+            log(`Starting item: ${attachItemId}`);
 
             let attachment = await Zotero.Items.getAsync(attachItemId);
             let attachType = attachment.attachmentContentType;
-            let libraryId  = Zotero.API.getLibraryPrefix(attachment.libraryID);
 
-            if (attachType === 'application/pdf') {
-              let pdfPath = attachment.getFilePath();
-              let pdfData = await Zotero.File.getBinaryContentsAsync(pdfPath);
-              let pdfDoc  = await pdfjsLib.getDocument({data : pdfData}).promise; 
-              let outline = await pdfDoc.getOutline();
+            if (attachType !== 'application/pdf')
+            {
+              log(`Not a valid PDF file. skipping ${attachItemId}`);
+              continue;
+            }
 
-              if (!outline) {
-                  window.alert(`No outline found. Quitting.`);
-                  return;
-              }
+            let attachPath  = attachment.getFilePath();
+            let attachDir   = Zotero.File.pathToFile(attachPath).parent.path;
+            let pdfBinary   = await Zotero.File.getBinaryContentsAsync(attachPath);
+            let pdfDocument = await pdfjsLib.getDocument({data : pdfBinary}).promise; 
+            let pdfOutline  = await pdfDocument.getOutline();
+            let outlinePath = Zotero.File.pathToFile(attachDir);
 
-              progressItem.setText(`Processing item ${item.getField('title')}...`);
+            outlinePath.append(outlineFileName);
 
-              let bookmarks = [];
+            outlinePath = outlinePath.path;
+            
+            if (Zotero.isWin) outlinePath = outlinePath.replace(/\\/g,"\\\\");
 
-              let maxdepth = window.prompt('Enter the maximum depth to look into the PDF bookmark.');
+            let txtOutline = await getAttachment(item, outlineFileName);
+            let bookmarks  = [];
 
-              maxdepth = parseInt(maxdepth);
+            progressItem.setText(`Processing item ${itemTitle}...`);
 
-              log(`Max depth value: ${maxdepth}`);
+            // check if outline note exists in item
+            if (txtOutline)
+            {
+              log(`outline-b2bs.txt file found. Creating bookmarks from file ${outlinePath}.`);
 
-              if (!Number.isInteger(maxdepth) || maxdepth <= 0) {
+              await parseOutlineFile(outlinePath, bookmarks);
+            }
+
+            // check if outline exists
+            if (pdfOutline && !txtOutline)
+            {
+              let maxDepth = window.prompt('Enter the maximum depth to look into the pdf bookmark.');
+
+              maxDepth = parseInt(maxDepth);
+
+              log(`Max depth value: ${maxDepth}`);
+
+              // End if maximum depth is neither an integer or is less than/equal to zero
+              if (!Number.isInteger(maxDepth) || maxDepth <= 0)
+              {
                 window.alert('Invalid depth value. Enter a positive integer.');
                 break;
               }
 
-              await BooksToSections.getBookmarks(pdfDoc, outline, bookmarks, maxdepth);
-
-              let progressIndex = 0;
-              let progressCount = bookmarks.length;
-
-              for (const bookmark of bookmarks) {
-                let { title, page } = bookmark;
-                let webLinkURL = `zotero://open-pdf/${libraryId}/items/${attachment.key}?page=${page}`;
-                let newSection = await BooksToSections.createBookSection(title, item, selCol.id);
-
-                await BooksToSections.addAttachment(newSection.id, webLinkURL);
-
-                progressIndex = progressIndex + 1;
-
-                let progressPct = Math.floor((progressIndex / progressCount) * 100);
-
-                progressItem.setText(`Finished ${progressIndex} of ${progressCount}`);
-                progressItem.setProgress(progressPct); 
-
-              }
-
-              progressItem.setText(`Finished ${item.getField('title')}`);
-              progressItem.setProgress(100);
-
-            } else {
-              log(`Not a valid PDF file. Skipping ${attachItemId}`);
-
+              await getBookmarksFromOutline(pdfDocument, pdfOutline, bookmarks, maxDepth);
             }
+
+            if (!pdfOutline && !txtOutline)
+            {
+              let createOutlineMsg = 'No outline found.\n'
+                                   + 'Press OK to create a note and add your outline there.\n'
+                                   + 'Press Cancel to abort.'
+              let createOutlineBln = window.confirm(createOutlineMsg);
+
+              // End if user presses cancel
+              if (!createOutlineBln) break;
+
+              let outlineContent = '# title (use quotes if there are spaces), page number\n'
+                                 + '# "Chapter 1", 1\n'
+                                 + '# "Chapter 2", 2\n'
+                                 + '# ...\n';
+
+              await Zotero.File.putContentsAsync(outlinePath, outlineContent);
+
+              await addAttachment(item.id, outlineFileName, outlinePath);
+
+              window.alert( `A file called \'${outlineFileName}\' has been added to this item.\n`
+                          + 'Edit this file to add your desired outline; the file will have examples.\n'
+                          + 'Lines starting with # will be ignored.'
+                          );
+              break;
+            }
+
+            let progressIndex = 0;
+            let progressCount = bookmarks.length;
+            let libraryId  = Zotero.API.getLibraryPrefix(attachment.libraryID);
+
+            for (const bookmark of bookmarks)
+            {
+              let { title, page } = bookmark;
+              let webLinkURL = `zotero://open-pdf/${libraryId}/items/${attachment.key}?page=${page}`;
+              let newSection = await createBookSection(title, item, selectedCollection.id);
+
+              await addAttachment(newSection.id, webLinkURL, webLinkURL);
+
+              progressIndex = progressIndex + 1;
+
+              let progressPct = Math.floor((progressIndex / progressCount) * 100);
+
+              progressItem.setText(`Finished ${progressIndex} of ${progressCount}`);
+              progressItem.setProgress(progressPct); 
+            }
+
+            progressItem.setText(`Finished ${item.getField('title')}`);
+            progressItem.setProgress(100);
+
           }
         }
-      } catch (e) {
+      }
+      catch (e)
+      {
         log('Error processing items', e);
         progressItem.setError();
 
-      } finally {
-        log('Ending process');
+      }
+      finally
+      {
+        log('Ending');
         progress.close();
 
       }
     });
 
-    log('Adding element to item menu');
-    menu.appendChild(menuitem);
-    this.storeAddedElement(menuitem);
+    log('Adding to item menu');
+    menu.appendChild(menuItem);
+    this.storeAddedElement(menuItem);
   },
 
-  removeFromWindow(window) {
+  removeFromWindow(window)
+  {
     let doc = window.document;
 
-    // Remove all elements with the stored IDs
-    this.addedElementIDs.forEach(id => {
+    this.addedElementIDs.forEach(id =>
+    {
       let elem = doc.getElementById(id);
-      if (elem) {
-        elem.remove();
-      }
+      if (elem) elem.remove();
     });
 
-    // Clear the added elements' IDs after removal
     this.addedElementIDs = [];
   },
 
-  addToAllWindows() {
+  addToAllWindows()
+  {
     var windows = Zotero.getMainWindows();
 
-    for (let win of windows) {
-      if (win.ZoteroPane) {
-        this.addToWindow(win);
-      }
+    for (let win of windows)
+    {
+      if (win.ZoteroPane) this.addToWindow(win);
     }
   },
 
-  removeFromAllWindows() {
+  removeFromAllWindows()
+  {
     let windows = Zotero.getMainWindows();
 
-    for (let win of windows) {
-      if (win.ZoteroPane) {
-        this.removeFromWindow(win);
-      }
+    for (let win of windows)
+    {
+      if (win.ZoteroPane) this.removeFromWindow(win);
     }
   },
 
-  storeAddedElement(elem) {
-    if (!elem.id) {
-      throw new Error("Element must have an id");
-    }
+  storeAddedElement(elem)
+  {
+    if (!elem.id) throw new Error("Element must have an id");
 
     this.addedElementIDs.push(elem.id);
   },
